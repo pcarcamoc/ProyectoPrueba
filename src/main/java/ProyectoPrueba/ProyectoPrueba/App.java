2301e4ccef6a841f058279e0fc4056c1e145b0dc
Pamela Carcamo <pamela.carcamo@tsoftglobal.com>
2019-02-19 17:50:53 -0300

    Prueba

---
+++ src/main/java/ProyectoPrueba/ProyectoPrueba/App.java
@@ -0,0 +1,13 @@
+package ProyectoPrueba.ProyectoPrueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
